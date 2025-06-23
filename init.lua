9c3f8103011748f98a384a1e61c166ae923db636
Matt Edwards <matt.edwards94@gmail.com>
2025-06-23 07:50:16 +0100

    Move lazydev to standalone file

--- init.lua
+++ init.lua
@@ -63,21 +63,6 @@ require('lazy').setup({
   'MattesGroeger/vim-bookmarks',
 
 
-  {
-    -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-    -- used for completion, annotations and signatures of Neovim apis
-    'folke/lazydev.nvim',
-    ft = 'lua',
-    opts = {
-      library = {
-        -- Load luvit types when the `vim.uv` word is found
-        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
-        { path = 'snacks.nvim', words = { 'Snacks' } },
-        { path = 'lazy.nvim', words = { 'LazyVim' } },
-      },
-    },
-  },
-
   -- { -- Autoformat
   --   'stevearc/conform.nvim',
   --   event = { 'BufWritePre' },
