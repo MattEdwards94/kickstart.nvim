d4d136e5be1f480f365d5eb2e07d23ccb93aa43d
Matt Edwards <matt.edwards94@gmail.com>
2025-07-05 09:28:01 +0100

    Add H and L for moving between open buffers

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -136,6 +136,10 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Use H and L to move between buffers
+vim.keymap.set('n', 'H', '<cmd>bprevious<cr>', { desc = 'Move to previous buffer' })
+vim.keymap.set('n', 'L', '<cmd>bnext<cr>', { desc = 'Move to next buffer' })
+
 -- toggle option
 Snacks.toggle.option('spell', { name = 'Spelling' }):map('<leader>us')
 Snacks.toggle.option('wrap', { name = 'Wrap' }):map('<leader>uw')
